6068b627b5a19fdaf19efce450333f70113c8092
chi793 <71268033+chi793@users.noreply.github.com>
2021-03-25 19:03:57 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,20 @@
+class Box {
+constructor(x,y,width,height) {
+ var options ={
+   restitution:1  
+ }  
+this.body = Bodies.rectangle(x,y,width,height,options);
+this.width=width ;
+
+World.add(world,this.body);
+}
+display(){
+    push();
+    translate(this.body.position.x,this.body.position.y);
+    rotate(this.body.angle);
+ rectMode(CENTER)  
+ fill (255)
+ rect(0,0,this.width,this.height);
+ pop();
+}
+};
